3df1ba918e26c3eb0bd10084f1da80bc893658cb
abattaglia <abattaglia1990@gmail.com>
2016-03-04 19:57:27 -0500

    Pickups/HealthPacks
    Unimplemented classes

---
+++ Assets/Scripts/HealthPack.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPack : MonoBehaviour {
+
+    public float healAmount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void HealHealth()
+    {
+        // Heal whoever picks up kit
+    }
+}
---
+++ Assets/Scripts/PickUps.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUps : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PickUp()
+    {
+        // If player is close display pickup key to pick up
+    }
+
+    void Drop()
+    {
+        // If items needs to be dropped
+    }
+
+
+}
