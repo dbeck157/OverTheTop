824a1c9ad53a6d91ba2ae1f5510a55c82eda27ad
abattaglia <abattaglia1990@gmail.com>
2016-03-04 20:31:15 -0500

    Made pickup script easier to read

--- Assets/Scripts/PickUpsScripts/PickUps.cs
+++ Assets/Scripts/PickUpsScripts/PickUps.cs
@@ -5,38 +5,40 @@ public class PickUps : MonoBehaviour {
 
     public Transform PlayerReference;
     public float pickUpDistance = 1;
+    public bool forcePickUp = false;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        PickUp();
-	}
+	void Update () 
+    {
+        if (CanPickUp())// If player is close display pickup key to pick up
+        {
+            DisplayPickUpInformation(); // Displays pickup information
+            if(Input.GetKeyDown(KeyCode.E) || forcePickUp) // if the player presses the key or the item is a force pick up pick it up
+            {
+                PickUp();
+            }
+        }
+    }
 
-    bool CanPickUp()
+    bool CanPickUp() // Checks the distance between pickup and player
     {
         float distance = Vector3.Distance(PlayerReference.position, transform.position);
 
         return distance < pickUpDistance;
     }
 
+    void DisplayPickUpInformation()
+    {
+        Debug.Log("Press E to pickup");
+    }
+
     void PickUp()
     {
-        // get distance between the pickup and the player
-
-        if (CanPickUp())// If player is close display pickup key to pick up
-        {
-            Debug.Log("Press E To Pickup");
-            // display pick up option
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-                Debug.Log("Picked up... sorta");
-                // If they press the key pick it up
-            }
-        }
-        
+        Debug.Log("Picked up... sorta");    
     }
 
     void Drop()
--- Assets/Scripts/PickUpsScripts/Weapon.cs
+++ Assets/Scripts/PickUpsScripts/Weapon.cs
@@ -5,6 +5,7 @@ public class Weapon : PickUps {
 
     public float rateOfFire;
     public float weaponPower;
+    public float weaponRange;
 
 	// Use this for initialization
 	void Start () {
