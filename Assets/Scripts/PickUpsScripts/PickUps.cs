3a949c6659c43e7815f494b797e68e4bd95c14f5
abattaglia <abattaglia1990@gmail.com>
2016-03-04 20:20:21 -0500

    Some implementation in pickups new weapon script
    put some implementation for pickups and created a new unimplemented
    weapon script

--- Assets/Scripts/PickUpsScripts/PickUps.cs
+++ Assets/Scripts/PickUpsScripts/PickUps.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PickUps : MonoBehaviour {
 
+    public Transform PlayerReference;
+    public float pickUpDistance = 1;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,17 +12,40 @@ public class PickUps : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        PickUp();
 	}
+
+    bool CanPickUp()
+    {
+        float distance = Vector3.Distance(PlayerReference.position, transform.position);
+
+        return distance < pickUpDistance;
+    }
 
     void PickUp()
     {
-        // If player is close display pickup key to pick up
+        // get distance between the pickup and the player
+
+        if (CanPickUp())// If player is close display pickup key to pick up
+        {
+            Debug.Log("Press E To Pickup");
+            // display pick up option
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Picked up... sorta");
+                // If they press the key pick it up
+            }
+        }
+        
     }
 
     void Drop()
     {
         // If items needs to be dropped
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            // If the player wants to drop the item?
+        }
     }
 
 
---
+++ Assets/Scripts/PickUpsScripts/Weapon.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : PickUps {
+
+    public float rateOfFire;
+    public float weaponPower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void PrimaryAttack()
+    {
+
+    }
+
+    void SecondaryAttack()
+    {
+
+    }
+}
